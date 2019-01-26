5d01dc894379ca47e8805da4a3c3551dde2db7b5
chashao <461006220@qq.com>
2019-01-26 17:25:37 +0800

    finish chapter 6

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -5,7 +5,7 @@ describe('Blcok', () => {
 
     beforeEach(() => {
         data = 'bar';
-        lastBlock = Block.genesis;
+        lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
     });
 
@@ -14,6 +14,18 @@ describe('Blcok', () => {
     });
 
     it('sets the `lastHash` to match the hash of the last block', () => {
-        expect(block.lastBlock).toEqual(lastBlock.hash);
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generates a hash that mactches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+    
+    it('lowers the difficulties for slowly mined block', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty - 1);
+    });
+
+    it('raises the difficulties for quickly mined block', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+1)).toEqual(block.difficulty + 1);
     });
 });
