12c24defad4326ae1b9f454577501ea800251bfc
chashao <461006220@qq.com>
2019-02-26 19:06:16 +0800

    makes some changes

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,96 +1,110 @@
 const Websocket = require('ws');
+const Ip = require('ip');
+const { NODES_IP } = require('../config');
+
 
 const MESSAGE_TYPES = {
-    chain: 'CHAIN',
-    transaction: 'TRANSACTION',
-    clear_transaction: 'CLEAR_TRANSACTION'
+  chain: 'CHAIN',
+  transaction: 'TRANSACTION',
+  clear_transaction: 'CLEAR_TRANSACTION'
 }
 
-const P2P_PORT = process.env.P2P_PORT || 5001;
-const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+const P2P_PORT = process.env.P2P_PORT || 5000;
+
+// const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+// const peers = () => {
+//   const hostIp = Ip.address();
+//   const OtherNodesIp = NODES_IP.filter(ip => ip !== hostIp);
+//   return OtherNodesIp.map(ws_address => {
+//     ws_address = "ws://" + ws_address + ":" + P2P_PORT;
+//   });
+// };
+
+const peers = NODES_IP.filter(ip => ip !== Ip.address())
+        .map(ws_address => "ws://" + ws_address + ":" + P2P_PORT);
 
 class P2pServer {
-    constructor(blockchain, transactionPool) {
-        this.blockchain = blockchain;
-        this.transactionPool = transactionPool;
-        this.sockets = [];
-    }
+  constructor(blockchain, transactionPool) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.sockets = [];
+  }
 
-    listen() {
-        const server = new Websocket.Server({ port: P2P_PORT });
-        server.on('connection', socket => this.connectSocket(socket));
+  listen() {
+    const server = new Websocket.Server({ port: P2P_PORT });
+    server.on('connection', socket => this.connectSocket(socket));
 
-        this.connectToPeers();
+    this.connectToPeers();
 
-        console.log(`Listening for peers-to-peers connections on: ${P2P_PORT}`);
-    }
+    console.log(`Listening for peers-to-peers connections on: ${P2P_PORT}`);
+  }
 
-    connectToPeers() {
-        peers.forEach(peer => {
-            const socket = new Websocket(peer);
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer);
 
-            socket.on('open', () => this.connectSocket(socket));
-        });
-    }
+      socket.on('open', () => this.connectSocket(socket));
+    });
+  }
 
-    connectSocket(socket) {
-        this.sockets.push(socket);
-        console.log('Socket connected');
+  connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log('Socket connected');
 
-        this.messageHandler(socket);
-        this.sendChain(socket);
+    this.messageHandler(socket);
+    this.sendChain(socket);
 
-    }
+  }
 
-    messageHandler(socket) {
-        socket.on('message', message => {
-            const data = JSON.parse(message);
-            switch(data.type) {
-                case MESSAGE_TYPES.chain:
-                    this.blockchain.replaceChain(data.chain);
-                    break;
-                case MESSAGE_TYPES.transaction :
-                    this.transactionPool.updateOrAddTransaction(data.transaction);
-                    break;
-                case MESSAGE_TYPES.clear_transaction :
-                    this.transactionPool.clear();
-                    break;
-            }
-        });
-    }
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      switch(data.type) {
+        case MESSAGE_TYPES.chain:
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case MESSAGE_TYPES.transaction :
+          this.transactionPool.updateOrAddTransaction(data.transaction);
+          break;
+        case MESSAGE_TYPES.clear_transaction :
+          this.transactionPool.clear();
+          break;
+      }
+    });
+  }
 
-    sendChain(socket) {
-        socket.send(JSON.stringify({
-            type: MESSAGE_TYPES.chain,
-            chain: this.blockchain.chain
-        }));
-    }
+  sendChain(socket) {
+    socket.send(JSON.stringify({
+      type: MESSAGE_TYPES.chain,
+      chain: this.blockchain.chain
+    }));
+  }
 
-    sendTransaction(socket, transaction) {
-        socket.send(JSON.stringify({
-            type: MESSAGE_TYPES.transaction,
-            // es6 (key: value) shorthand
-            // with property value shorthand
-            // syntax, you can omit the property
-            // value if key matches variable
-            // name
-            transaction
-        }));
-    }
+  sendTransaction(socket, transaction) {
+    socket.send(JSON.stringify({
+      type: MESSAGE_TYPES.transaction,
+      // es6 (key: value) shorthand
+      // with property value shorthand
+      // syntax, you can omit the property
+      // value if key matches variable
+      // name
+      transaction
+    }));
+  }
 
-    syncChain() {
-        this.sockets.forEach(socket => this.sendChain(socket));
-    }
+  syncChain() {
+    this.sockets.forEach(socket => this.sendChain(socket));
+  }
 
-    broadcastTransaction(transaction) {
-        this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
-    }
+  broadcastTransaction(transaction) {
+    this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+  }
 
-    broadcastClearTransactions() {
-        this.sockets.forEach(socket => socket.send(JSON.stringify({
-            type: MESSAGE_TYPES.clear_transaction
-        })));
-    }
+  broadcastClearTransactions() {
+    this.sockets.forEach(socket => socket.send(JSON.stringify({
+      type: MESSAGE_TYPES.clear_transaction
+    })));
+  }
 }
 
 module.exports = P2pServer;
--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -4,58 +4,58 @@ const Blockchain = require('../blockchain');
 
 
 describe('TransactioPool', () => {
-    let tp, wallet, transaction, bc;
+  let tp, wallet, transaction, bc;
+  beforeEach(() => {
+    tp = new TransactionPool;
+    wallet = new Wallet;
+    bc = new Blockchain()
+    transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
+  });
+
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction);
+  });
+  
+  it('updates a transaction to the pool', () => {
+    const oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update(wallet, 'foo-4dddr355', 40);
+    tp.updateOrAddTransaction(newTransaction);
+
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id)))
+      .not.toEqual(oldTransaction);
+  });
+
+  it('clears transaction pool', () => {
+    tp.clear();
+    expect(tp.transactions).toEqual([]);
+  });
+
+  describe('mixing valid and corrupt transactions', () => {
+    let validTransactions;
+
     beforeEach(() => {
-        tp = new TransactionPool;
-        wallet = new Wallet;
-        bc = new Blockchain()
-        transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
+      validTransactions = [...tp.transactions];
+
+      for(let i = 0; i < 6; i++) {
+        wallet = new Wallet();
+        transaction = wallet.createTransaction('r4nd-4ddr355', 30, bc, tp);
+        if(i % 2 == 0) {
+          transaction.input.amount = 99999;
+        } else {
+          validTransactions.push(transaction);
+        }
+      }
     });
 
-    it('adds a transaction to the pool', () => {
-        expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction);
-    });
-    
-    it('updates a transaction to the pool', () => {
-        const oldTransaction = JSON.stringify(transaction);
-        const newTransaction = transaction.update(wallet, 'foo-4dddr355', 40);
-        tp.updateOrAddTransaction(newTransaction);
 
-        expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id)))
-            .not.toEqual(oldTransaction);
+    it('shows a difference between valid and corrupt transactions', () => {
+      expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
     });
 
-    it('clears transaction pool', () => {
-        tp.clear();
-        expect(tp.transactions).toEqual([]);
+    it('grabs valid transations', () => {
+      expect(JSON.stringify(tp.validTransactions())).toEqual(JSON.stringify(validTransactions));
     });
-
-    describe('mixing valid and corrupt transactions', () => {
-        let validTransactions;
-
-        beforeEach(() => {
-            validTransactions = [...tp.transactions];
-
-            for(let i = 0; i < 6; i++) {
-                wallet = new Wallet();
-                transaction = wallet.createTransaction('r4nd-4ddr355', 30, bc, tp);
-                if(i % 2 == 0) {
-                    transaction.input.amount = 99999;
-                } else {
-                    validTransactions.push(transaction);
-                }
-            }
-        });
-
-
-        it('shows a difference between valid and corrupt transactions', () => {
-            expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
-        });
-
-        it('grabs valid transations', () => {
-            expect(JSON.stringify(tp.validTransactions())).toEqual(JSON.stringify(validTransactions));
-        });
-    });
+  });
 
 
 
