a1be993cb1369fdcf165e3ad48d0245985b5ad7e
chashao <461006220@qq.com>
2019-03-30 11:37:26 +0800

    start from the original tutorial version

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 const DIFFICULTY = 3;
 const MINE_RATE = 3000;
-module.exports = { DIFFICULTY, MINE_RATE };
+const INITIAL_BALANCE = 500;
+const MINING_REWARD = 50;
+const NODES_IP = [
+        '192.168.99.102',
+        '192.168.99.103'
+      ]
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD, NODES_IP };
